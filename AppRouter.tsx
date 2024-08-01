b8c8d162fd8d34ac5cb0603560af69b018255861
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-08-01 23:11:48 +0300

    first commit

---
+++ AppRouter.tsx
@@ -0,0 +1,13 @@
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+import App from './App';
+
+const AppRouter = () => {
+  return (
+    <NavigationContainer>
+      <App />
+    </NavigationContainer>
+  );
+};
+
+export default AppRouter;
