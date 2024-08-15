e24a823b05df8dcfd1cbce3108beeb9f4f45540b
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-08-15 14:40:36 +0300

    refactor: Update import statements in index.js for better code organization

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
-import { name as appName } from './app.json';
+import {AppRegistry} from 'react-native';
+import {name as appName} from './app.json';
 import App from './src/AppProvider';
 
 AppRegistry.registerComponent(appName, () => App);
