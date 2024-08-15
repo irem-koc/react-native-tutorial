f782de071461d553b45cf755962e91d2e721e038
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-08-15 22:52:42 +0300

    feat: Update BarcodeReader component to use react-native-vision-camera for barcode scanning

--- src/pages/auth/BarcodeReader/BarcodeReader.tsx
+++ src/pages/auth/BarcodeReader/BarcodeReader.tsx
@@ -1,12 +1,66 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text} from 'react-native';
+import {
+  Camera,
+  useCameraDevice,
+  useCameraPermission,
+  useCodeScanner,
+} from 'react-native-vision-camera';
 
 const BarcodeReader = () => {
+  const {hasPermission, requestPermission} = useCameraPermission();
+  //   const [scanFrame, setScanFrame] = useState<CodeScannerFrame>({
+  //     height: 1,
+  //     width: 1,
+  //   });
+
+  //   const [layout, setLayout] = useState<
+  //     LayoutChangeEvent['nativeEvent']['layout']
+  //   >({x: 0, y: 0, width: 0, height: 0});
+  const device = useCameraDevice('back');
+
+  const codeScanner = useCodeScanner({
+    codeTypes: ['qr', 'code-128'],
+    onCodeScanned: codes => {
+      codes.forEach(code => {
+        console.log(`Scanned code type: ${code.type}`);
+        console.log(`Scanned code value: ${code.value}`);
+      });
+    },
+  });
+
+  //   const onLayout = (evt: LayoutChangeEvent) => {
+  //     if (evt.nativeEvent.layout) {
+  //       setLayout(evt.nativeEvent.layout);
+  //     }
+  //   };
+
+  if (device == null) return <Text>Device not found</Text>;
+
   return (
-    <View>
-      <Text>BarcodeReader</Text>
-    </View>
+    <>
+      <Camera
+        style={StyleSheet.absoluteFill}
+        device={device}
+        isActive={true}
+        // onLayout={onLayout}
+        codeScanner={codeScanner}
+      />
+    </>
   );
 };
-
 export default BarcodeReader;
+export const styles = StyleSheet.create({
+  safeArea: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+  },
+  fullScreenCamera: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    flex: 1,
+    zIndex: 100,
+  },
+});
