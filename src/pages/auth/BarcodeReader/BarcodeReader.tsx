0409d96a0084503c69d1eac7d2f777f2fdc39072
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-08-15 21:21:23 +0300

    feat: Create BarcodeReader component for scanning barcodes

---
+++ src/pages/auth/BarcodeReader/BarcodeReader.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const BarcodeReader = () => {
+  return (
+    <View>
+      <Text>BarcodeReader</Text>
+    </View>
+  );
+};
+
+export default BarcodeReader;
